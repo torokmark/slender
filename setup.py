190efe6d7fd760db31701f46db6e0b2a280ab49a
Torok Mark <torok.marko@gmail.com>
2019-09-03 12:18:33 +0200

    bump minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='slender',
-      version='0.1.3.3',
+      version='0.1.3.7',
       description='Slender datastructures in Python for efficient work!',
       long_description=long_description,
       long_description_content_type='text/markdown',
